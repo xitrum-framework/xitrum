0429b155441fc686b03f83535a5098a45b47116b
Ngoc <ngoc@ngoc-ubuntu.(none)>
2010-07-30 14:44:22 +0900

    Uplevel package colinh to project

--- src/main/scala/colinh/controller/Application.scala
+++
@@ -1,7 +0,0 @@
-package colinh.controller
-
-import st.framework.Controller
-
-class Application extends Controller {
-  override def layout = Some("Layouts#application")
-}
--- src/main/scala/colinh/model/User.scala
+++
@@ -1,9 +0,0 @@
-package colinh.model
-
-import org.squeryl.PrimitiveTypeMode._
-
-class User(
-    var id:       Int,
-    var username: String) {
-  def this() = this(0, "")
-}
