f1981d83c7651860cf14d0df1e191a4e2e729a9f
Dao Ngoc <ngoc@Dao-Ngocs-Mac-mini.local>
2010-10-13 18:05:05 +0900

    Split middleware for Squeryl to a xitrum-squeryl subproject

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,6 +1,6 @@
 import sbt._
 
-class Project(info: ProjectInfo) extends DefaultProject(info) {
+class Project(info: ProjectInfo) extends ParentProject(info) {
   val localMavenRepo = "Local Maven Repo" at
     "file://" + Path.userHome + "/.m2/repository"
 
@@ -10,14 +10,23 @@ class Project(info: ProjectInfo) extends DefaultProject(info) {
   val scalateRepo = "Scalate Repo" at
     "http://repo.fusesource.com/nexus/content/repositories/snapshots"
 
-  override def libraryDependencies =
-    Set(
-      "org.slf4j"              % "slf4j-log4j12"        % "1.6.1"         % "compile->default",
-      "org.jboss.netty"        % "netty"                % "3.2.2.Final"   % "compile->default",
-      "org.fusesource.scalate" % "scalate-core"         % "1.3"           % "compile->default",
-      "org.squeryl"            % "squeryl_2.8.0"        % "0.9.4-RC2"     % "compile->default",
-      "c3p0"                   % "c3p0"                 % "0.9.1"         % "compile->default",
-      "postgresql"             % "postgresql"           % "8.4-701.jdbc4" % "compile->default",
-      "mysql"                  % "mysql-connector-java" % "5.1.13"        % "compile->default"
-    ) ++ super.libraryDependencies
+  lazy val core    = project("core",    "xitrum-core",    new Core(_))
+  lazy val squeryl = project("squeryl", "xitrum-squeryl", new Squeryl(_), core)
+
+  class Core(info: ProjectInfo) extends DefaultProject(info) {
+    override def libraryDependencies =
+      Set(
+        "org.slf4j"              % "slf4j-log4j12" % "1.6.1"       % "compile->default",
+        "org.jboss.netty"        % "netty"         % "3.2.2.Final" % "compile->default",
+        "org.fusesource.scalate" % "scalate-core"  % "1.3"         % "compile->default"
+      ) ++ super.libraryDependencies
+  }
+
+  class Squeryl(info: ProjectInfo) extends DefaultProject(info) {
+    override def libraryDependencies =
+      Set(
+        "org.squeryl" % "squeryl_2.8.0" % "0.9.4-RC2" % "compile->default",
+        "c3p0"        % "c3p0"          % "0.9.1"     % "compile->default"
+      ) ++ super.libraryDependencies
+  }
 }
