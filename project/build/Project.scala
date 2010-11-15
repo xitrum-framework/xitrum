111c803170077d2bdfe2691689447a31968c618b
Ngoc <ngoc@ngoc-ubuntu.(none)>
2010-11-15 09:51:21 +0900

    Update Ehacache to 2.3.0

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -22,8 +22,7 @@ class Project(info: ProjectInfo) extends DefaultProject(info) {
   val scalate = "Scalate" at
     "http://repo.fusesource.com/nexus/content/repositories/snapshots"
 
-  val terracotta = "Terracotta" at
-    "http://www.terracotta.org/download/reflector/releases"
+  val ehcache = "Ehcache" at "https://oss.sonatype.org/content/repositories/sourceforge-releases"
 
   val reflections = "Reflections" at "http://reflections.googlecode.com/svn/repo"
 
@@ -31,10 +30,10 @@ class Project(info: ProjectInfo) extends DefaultProject(info) {
   lazy val squeryl = project("squeryl", "xitrum-squeryl", new Squeryl(_), core)
 
   class Core(info: ProjectInfo) extends DefaultProject(info) {
-    // Use SLF4J. Projects using Xitrum must provide a concrete implentation (Logback etc.).
     override def libraryDependencies =
       Set(
-        // Log
+        // Log using SLF4J
+        // Projects using Xitrum must provide a concrete implentation (Logback etc.).
         "org.slf4j"              % "slf4j-api"    % "1.6.1",
 
         // Web server
@@ -47,17 +46,17 @@ class Project(info: ProjectInfo) extends DefaultProject(info) {
         "org.reflections"        % "reflections"  % "0.9.5-RC2",  // Uses Javassist
 
         // For sessions and caching files
-        "net.sf.ehcache"    % "ehcache" % "2.2.0",
-        "javax.transaction" % "jta"     % "1.1"  // Used by Ehcache
+        "net.sf.ehcache"         % "ehcache-core" % "2.3.0"
       ) ++ super.libraryDependencies
   }
 
   class Squeryl(info: ProjectInfo) extends DefaultProject(info) {
-    // http://www.mchange.com/projects/c3p0/index.html#configuring_logging
-    // Redirect c3p0 log to SLF4J (Logback, see above)
     override def libraryDependencies =
       Set(
         "org.squeryl" % "squeryl_2.8.0"    % "0.9.4-RC2",
+
+        // http://www.mchange.com/projects/c3p0/index.html#configuring_logging
+        // Redirect c3p0 log to SLF4J (Logback, see above)
         "c3p0"        % "c3p0"             % "0.9.1",
         "org.slf4j"   % "log4j-over-slf4j" % "1.6.1"
       ) ++ super.libraryDependencies
