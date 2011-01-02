33a88838dde8a8a666349dc1723ae5250e763a5e
Ngoc Dao <ngoc@ngoc-2.local>
2011-01-02 14:34:32 +0900

    Use lazy to avoid scala.UninitializedFieldError

--- src/main/scala/xt/Config.scala
+++ src/main/scala/xt/Config.scala
@@ -7,33 +7,34 @@ import net.sf.ehcache.CacheManager
 
 import xt.vc.env.session.SessionStore
 
+// Use lazy to avoid scala.UninitializedFieldError
 object Config {
-  val isProductionMode = (System.getProperty("xitrum.mode") == "production")
+  lazy val isProductionMode = (System.getProperty("xitrum.mode") == "production")
 
-  val httpPort         = properties.getProperty("http_port",          "8080").toInt
-  val maxContentLength = properties.getProperty("max_content_length", "1048576").toInt  // default: 10MB
+  lazy val httpPort         = properties.getProperty("http_port",          "8080").toInt
+  lazy val maxContentLength = properties.getProperty("max_content_length", "1048576").toInt  // default: 10MB
 
-  val paramCharsetName = properties.getProperty("param_charset", "UTF-8")
-  val paramCharset     = Charset.forName(paramCharsetName)
+  lazy val paramCharsetName = properties.getProperty("param_charset", "UTF-8")
+  lazy val paramCharset     = Charset.forName(paramCharsetName)
 
-  val filterParams = properties.getProperty("filter_params", "password").split(", ")
+  lazy val filterParams = properties.getProperty("filter_params", "password").split(", ")
 
-  val filesEhcacheName = properties.getProperty("files_ehcache_name", "XitrumFiles")
-  val filesMaxSize     = properties.getProperty("files_max_size",     "102400").toInt
+  lazy val filesEhcacheName = properties.getProperty("files_ehcache_name", "XitrumFiles")
+  lazy val filesMaxSize     = properties.getProperty("files_max_size",     "102400").toInt
 
-  val sessionIdName         = properties.getProperty("session_id_name",           "JSESSIONID")
-  val sessionIdInCookieOnly = properties.getProperty("session_id_in_cookie_only", "true") == "true"
-  val sessionStore: SessionStore = {
+  lazy val sessionIdName         = properties.getProperty("session_id_name",           "JSESSIONID")
+  lazy val sessionIdInCookieOnly = properties.getProperty("session_id_in_cookie_only", "true") == "true"
+  lazy val sessionStore: SessionStore = {
     val className = properties.getProperty("session_store", "xt.vc.env.session.EhcacheSessionStore")
     Class.forName(className).newInstance.asInstanceOf[SessionStore]
   }
 
-  val sessionEhcache = {
+  lazy val sessionEhcache = {
     val name = Config.properties.getProperty("sessions_ehcache_name", "XitrumSessions")
     CacheManager.getInstance.getCache(name)
   }
 
-  private val properties = {
+  private lazy val properties = {
     val ret = new Properties
     val stream = getClass.getClassLoader.getResourceAsStream("xitrum.properties")
     if (stream != null) ret.load(stream)
