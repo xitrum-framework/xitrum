41e3893317592ff4dde52f288c51ba88b0f52dd1
Ngoc Dao <ngoc@ngoc-2.local>
2011-06-23 01:58:22 +0900

    Decrease default MaxPermSize from 512m to 128m

--- plugin/src/main/resources/xitrum_resources/bin/runner.sh
+++ plugin/src/main/resources/xitrum_resources/bin/runner.sh
@@ -3,7 +3,7 @@
 ROOT_DIR="$(cd "$(dirname "$0")/.." && pwd)"
 cd "$ROOT_DIR"
 
-JAVA_OPTS='-Xms512m -Xmx1024m -XX:MaxPermSize=512m -server -Djava.awt.headless=true -Dxitrum.mode=production'
+JAVA_OPTS='-Xms512m -Xmx1024m -XX:MaxPermSize=128m -server -Djava.awt.headless=true -Dxitrum.mode=production'
 
 # slf4j-api is put to the front to avoid error:
 # The requested version 1.6 by your slf4j binding is not compatible with [1.5.5, 1.5.6, 1.5.7, 1.5.8]
