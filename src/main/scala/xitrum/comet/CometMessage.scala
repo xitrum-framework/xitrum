97ff68901640d82dcc21400516c1410178154f95
Ngoc Dao <ngoc@ngoc-2.local>
2011-08-23 08:10:13 +0900

    CometMessage needs to be Serializable to be stored in Hazelcast

--- src/main/scala/xitrum/comet/CometMessage.scala
+++ src/main/scala/xitrum/comet/CometMessage.scala
@@ -2,4 +2,5 @@ package xitrum.comet
 
 import xitrum.scope.request.Params
 
-class CometMessage(val channel: String, val timestamp: Long, val body: Params)
+// Needs to be Serializable to be stored in Hazelcast
+class CometMessage(val channel: String, val timestamp: Long, val body: Params) extends Serializable
