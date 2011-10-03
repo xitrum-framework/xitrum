0b9addaad0039acf9a0278c2bf7d442439380b01
Ngoc Dao <ngocdaothanh@gmail.com>
2011-10-04 04:43:41 +0900

    Check Accept-Encoding

--- src/main/scala/xitrum/util/Gzip.scala
+++ src/main/scala/xitrum/util/Gzip.scala
@@ -3,7 +3,15 @@ package xitrum.util
 import java.io.ByteArrayOutputStream
 import java.util.zip.GZIPOutputStream
 
+import org.jboss.netty.handler.codec.http.{HttpHeaders, HttpRequest}
+import HttpHeaders.Names.ACCEPT_ENCODING
+
 object Gzip {
+  def isAccepted(request: HttpRequest) = {
+    val acceptEncoding = request.getHeader(ACCEPT_ENCODING)
+    (acceptEncoding != null && acceptEncoding.contains("gzip"))
+  }
+
   def compress(bytes: Array[Byte]) = {
     val b = new ByteArrayOutputStream
     val g = new GZIPOutputStream(b)
--- src/main/scala/xitrum/view/JSRoutesAction.scala
+++ src/main/scala/xitrum/view/JSRoutesAction.scala
@@ -13,38 +13,41 @@ import xitrum.routing.Routes
 import xitrum.util.Gzip
 
 object JSRoutesAction {
-  private var gzipped: Array[Byte] = null
+  private var js:        String      = null
+  private var gzippedJs: Array[Byte] = null
 
-  def jsRoutes(action: Action): String = {
-    "var XITRUM_BASE_URI = '" + Config.baseUri + "';\n" +
-    "var XITRUM_ROUTES = " + Routes.jsRoutes + ";\n" +
-    "var XITRUM_COMET_GET_ACTION = '" + action.urlForPostback[CometGetAction] + "';"
+  def jsRoutes(action: Action): String = synchronized {
+    if (js == null) {
+      js =
+        "var XITRUM_BASE_URI = '" + Config.baseUri + "';\n" +
+        "var XITRUM_ROUTES = " + Routes.jsRoutes + ";\n" +
+        "var XITRUM_COMET_GET_ACTION = '" + action.urlForPostback[CometGetAction] + "';"
+    }
+    js
   }
 
   def gzippedJsRoutes(action: Action): Array[Byte] = synchronized {
-    if (gzipped == null) {
-      val js  = jsRoutes(action)
-      gzipped = Gzip.compress(js.getBytes(Config.paramCharsetName))
+    if (gzippedJs == null) {
+      gzippedJs = Gzip.compress(jsRoutes(action).getBytes(Config.paramCharsetName))
     }
-    gzipped
+    gzippedJs
   }
+
+  // This value is stable, even across different servers in a cluster
+  lazy val etag = Etag.forString(Routes.jsRoutes)
 }
 
 @GET("/xitrum/routes.js")
 class JSRoutesAction extends Action {
   override def execute {
-    // See PublicResourceServerAction
-    val etag = Etag.forBytes(Routes.jsRoutes.getBytes)
-    if (!Etag.respondIfEtagsIdentical(this, etag)) {
-      response.setHeader(ETAG, etag)
+    if (!Etag.respondIfEtagsIdentical(this, JSRoutesAction.etag)) {
       NotModified.setMaxAgeAggressively(response)
-
-      if (Config.isProductionMode) {
-        jsRender(JSRoutesAction.jsRoutes(this))
-      } else {
-        response.setHeader(CONTENT_TYPE, "text/javascript")
+      response.setHeader(CONTENT_TYPE, "text/javascript")
+      if (Gzip.isAccepted(request)) {
         response.setHeader(CONTENT_ENCODING, "gzip")
         renderBinary(JSRoutesAction.gzippedJsRoutes(this))
+      } else {
+        jsRender(JSRoutesAction.jsRoutes(this))
       }
     }
   }
