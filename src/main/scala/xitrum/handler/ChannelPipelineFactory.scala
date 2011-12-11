1f8aaec4fb4fc859bf320909b2dfb28650de81f9
Ngoc Dao <ngocdaothanh@gmail.com>
2011-12-12 03:16:44 +0900

    New pipeline is created for every new connection

--- src/main/scala/xitrum/handler/ChannelPipelineFactory.scala
+++ src/main/scala/xitrum/handler/ChannelPipelineFactory.scala
@@ -10,6 +10,24 @@ import xitrum.handler.down._
 import xitrum.handler.updown._
 
 object ChannelPipelineFactory {
+  def removeUnusedDefaultHttpHandlersForWebSocket(pipeline: ChannelPipeline) {
+    pipeline.remove(classOf[XSendFile])
+    pipeline.remove(classOf[XSendResource])
+    pipeline.remove(classOf[BaseUriRemover])
+    pipeline.remove(classOf[PublicFileServer])
+    pipeline.remove(classOf[PublicResourceServer])
+    pipeline.remove(classOf[Request2Env])
+    pipeline.remove(classOf[UriParser])
+    pipeline.remove(classOf[BodyParser])
+    pipeline.remove(classOf[MethodOverrider])
+    pipeline.remove(classOf[Dispatcher])
+    pipeline.remove(classOf[Env2Response])
+    pipeline.remove(classOf[ResponseCacher])
+  }
+}
+
+// See doc/sphinx/handler.rst
+class ChannelPipelineFactory(https: Boolean) extends CPF {
   // Sharable handlers
   private val baseUriRemover       = new BaseUriRemover
   private val publicFileServer     = new PublicFileServer
@@ -40,7 +58,32 @@ object ChannelPipelineFactory {
           corePoolSize, maxChannelMemorySize, maxTotalMemorySize))
   }
 
-  val defaultHttpHandlers = List(
+  def getPipeline: ChannelPipeline = {
+    val handlers1 = httpHandlers
+    val handlers2 = if (https) ServerSsl.handler +: handlers1 else handlers1
+
+    // StaticChannelPipeline provides extreme performance at the cost of
+    // disabled dynamic manipulation of pipeline
+    //
+    // Creating StaticChannelPipeline with empty constructor will cause
+    // java.lang.IllegalArgumentException: no handlers specified
+    //new StaticChannelPipeline(handlers2:_*)
+
+    // Cannot use StaticChannelPipeline because WebSocket handshaker in Netty
+    // dynamically changes the pipeline like this:
+    // pipeline.remove(classOf[HttpChunkAggregator])
+    // pipeline.replace(classOf[HttpRequestDecoder], "wsdecoder", new WebSocket08FrameDecoder(true, this.allowExtensions))
+    // pipeline.replace(classOf[HttpResponseEncoder], "wsencoder", new WebSocket08FrameEncoder(false))
+    Channels.pipeline(handlers2:_*)
+  }
+
+  /**
+   * You can override this method to customize the default pipeline.
+   *
+   * Upstream direction: first handler -> last handler
+   * Downstream direction: last handler -> first handler
+   */
+  def httpHandlers = List(
     // Up
     new HttpRequestDecoder,
     new HttpChunkAggregator(Config.config.request.maxSizeInMB * 1024 * 1024),
@@ -68,49 +111,4 @@ object ChannelPipelineFactory {
     env2Response,
     responseCacher
   )
-
-  def removeUnusedDefaultHttpHandlersForWebSocket(pipeline: ChannelPipeline) {
-    pipeline.remove(classOf[XSendFile])
-    pipeline.remove(classOf[XSendResource])
-    pipeline.remove(classOf[BaseUriRemover])
-    pipeline.remove(classOf[PublicFileServer])
-    pipeline.remove(classOf[PublicResourceServer])
-    pipeline.remove(classOf[Request2Env])
-    pipeline.remove(classOf[UriParser])
-    pipeline.remove(classOf[BodyParser])
-    pipeline.remove(classOf[MethodOverrider])
-    pipeline.remove(classOf[Dispatcher])
-    pipeline.remove(classOf[Env2Response])
-    pipeline.remove(classOf[ResponseCacher])
-  }
-}
-
-// See doc/sphinx/handler.rst
-class ChannelPipelineFactory(https: Boolean) extends CPF {
-  def getPipeline: ChannelPipeline = {
-    val handlers1 = httpHandlers
-    val handlers2 = if (https) ServerSsl.handler +: handlers1 else handlers1
-
-    // StaticChannelPipeline provides extreme performance at the cost of
-    // disabled dynamic manipulation of pipeline
-    //
-    // Creating StaticChannelPipeline with empty constructor will cause
-    // java.lang.IllegalArgumentException: no handlers specified
-    //new StaticChannelPipeline(handlers2:_*)
-
-    // Cannot use StaticChannelPipeline because WebSocket handshaker in Netty
-    // dynamically changes the pipeline like this:
-    // pipeline.remove(classOf[HttpChunkAggregator])
-    // pipeline.replace(classOf[HttpRequestDecoder], "wsdecoder", new WebSocket08FrameDecoder(true, this.allowExtensions))
-    // pipeline.replace(classOf[HttpResponseEncoder], "wsencoder", new WebSocket08FrameEncoder(false))
-    Channels.pipeline(handlers2:_*)
-  }
-
-  /**
-   * You can override this method to customize the default pipeline.
-   *
-   * Upstream direction: first handler -> last handler
-   * Downstream direction: last handler -> first handler
-   */
-  def httpHandlers = ChannelPipelineFactory.defaultHttpHandlers
 }
