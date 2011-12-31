a088d8a42d7f7bc096f357c64f31e69c327ab1c7
Ngoc Dao <ngocdaothanh@gmail.com>
2011-12-31 16:39:54 +0900

    Turn ScamlOptions.ugly on in production mode

--- src/main/scala/xitrum/view/Scalate.scala
+++ src/main/scala/xitrum/view/Scalate.scala
@@ -2,14 +2,16 @@ package xitrum.view
 
 import java.io.{ByteArrayOutputStream, PrintWriter}
 import org.fusesource.scalate.{Binding, DefaultRenderContext, TemplateEngine}
+import org.fusesource.scalate.scaml.ScamlOptions
 
 import xitrum.{Config, Action}
 
 object Scalate {
   lazy val engine = {
     val ret = new TemplateEngine
-    ret.allowReload = !Config.isProductionMode
-    ret.bindings    = List(Binding("helper", classOf[Action].getName, true))
+    ret.allowReload   = !Config.isProductionMode
+    ScamlOptions.ugly = Config.isProductionMode
+    ret.bindings      = List(Binding("helper", classOf[Action].getName, true))
     ret
   }
 
