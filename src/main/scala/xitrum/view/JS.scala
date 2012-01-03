f5c156103e56da09e901918538b954820ca3776e
Ngoc Dao <ngocdaothanh@gmail.com>
2012-01-03 19:22:28 +0900

    Update jQuery from 1.6.4 to 1.7.1

--- src/main/scala/xitrum/view/JS.scala
+++ src/main/scala/xitrum/view/JS.scala
@@ -81,7 +81,7 @@ trait JS {
 
     if (Config.isProductionMode)
       <xml:group>
-        <script type="text/javascript" src={urlForResource("xitrum/jquery-1.6.4.min.js")}></script>
+        <script type="text/javascript" src={urlForResource("xitrum/jquery-1.7.1.min.js")}></script>
         <script type="text/javascript" src={urlForResource("xitrum/jquery.validate-1.9.0/jquery.validate.min.js")}></script>
         <script type="text/javascript" src={urlForResource("xitrum/jquery.validate-1.9.0/additional-methods.min.js")}></script>
         {validatei18n}
@@ -93,7 +93,7 @@ trait JS {
       </xml:group>
     else
       <xml:group>
-        <script type="text/javascript" src={urlForResource("xitrum/jquery-1.6.4.js")}></script>
+        <script type="text/javascript" src={urlForResource("xitrum/jquery-1.7.1.js")}></script>
         <script type="text/javascript" src={urlForResource("xitrum/jquery.validate-1.9.0/jquery.validate.js")}></script>
         <script type="text/javascript" src={urlForResource("xitrum/jquery.validate-1.9.0/additional-methods.js")}></script>
         {validatei18n}
