e6df2a1032ec502b8e16c447594bd2c7887c6a3e
Dao Ngoc <ngocdaothanh@Daos-MacBook-Pro.local>
2012-08-14 15:21:40 +0900

    Optimize FlashSocketPolicyRequestDecoder

--- src/main/scala/xitrum/handler/up/FlashSocketPolicyRequestDecoder.scala
+++ src/main/scala/xitrum/handler/up/FlashSocketPolicyRequestDecoder.scala
@@ -7,7 +7,7 @@ import org.jboss.netty.handler.codec.frame.FrameDecoder
 object FlashSocketPolicyRequestDecoder {
   // The request must be exactly "<policy-file-request/>\0"
   val REQUEST                = ChannelBuffers.wrappedBuffer("<policy-file-request/>\0".getBytes)
-  val REQUEST_LENGTH         = "<policy-file-request/>\0".length
+  val REQUEST_LENGTH         = REQUEST.readableBytes
   val TICKET_TO_NEXT_HANDLER = new Object
 }
 
@@ -16,10 +16,10 @@ class FlashSocketPolicyRequestDecoder extends FrameDecoder {
   import FlashSocketPolicyRequestDecoder._
 
   protected override def decode(ctx: ChannelHandlerContext, channel: Channel, buffer: ChannelBuffer): Object = {
-    if (buffer.readableBytes() < REQUEST_LENGTH) {
+    if (buffer.readableBytes < REQUEST_LENGTH) {
       null
     } else {
-      // Check if the request is exactly "<policy-file-request/>\0"
+      // The request must be exactly "<policy-file-request/>\0"
       val channelBuffer = buffer.readBytes(REQUEST_LENGTH)
       if (channelBuffer.equals(REQUEST)) {
         TICKET_TO_NEXT_HANDLER
