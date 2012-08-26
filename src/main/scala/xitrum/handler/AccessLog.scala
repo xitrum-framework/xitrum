26b431550c7101a0b5953bc9c2780a76c3c40dac
Dao Ngoc <ngocdaothanh@ngoc.local>
2012-08-26 22:00:28 +0900

    Optimize by creating less object every request

--- src/main/scala/xitrum/handler/AccessLog.scala
+++ src/main/scala/xitrum/handler/AccessLog.scala
@@ -29,39 +29,41 @@ object AccessLog extends Logger {
   }
 
   def logDynamicContentAccess(controller: Controller, beginTimestamp: Long, cacheSecs: Int, hit: Boolean, e: Throwable = null) {
-    def msgWithTime = {
-      val endTimestamp = System.currentTimeMillis()
-      val dt           = endTimestamp - beginTimestamp
-      val env          = controller.handlerEnv
-
-      controller.remoteIp + " " +
-      controller.request.getMethod + " " +
-      controller.request.getUri + " -> " +
-      ControllerReflection.controllerActionName(controller.handlerEnv.action) +
-      (if (env.uriParams.nonEmpty)        ", uriParams: "        + RequestEnv.inspectParamsWithFilter(env.uriParams       .asInstanceOf[MMap[String, List[Any]]]) else "") +
-      (if (env.bodyParams.nonEmpty)       ", bodyParams: "       + RequestEnv.inspectParamsWithFilter(env.bodyParams      .asInstanceOf[MMap[String, List[Any]]]) else "") +
-      (if (env.pathParams.nonEmpty)       ", pathParams: "       + RequestEnv.inspectParamsWithFilter(env.pathParams      .asInstanceOf[MMap[String, List[Any]]]) else "") +
-      (if (env.fileUploadParams.nonEmpty) ", fileUploadParams: " + RequestEnv.inspectParamsWithFilter(env.fileUploadParams.asInstanceOf[MMap[String, List[Any]]]) else "") +
-      " -> " + controller.response.getStatus.getCode +
-      ", " + dt + " [ms]"
-    }
-
-    def extraInfo = {
-      if (cacheSecs == 0) {
-        if (controller.isResponded) "" else " (async)"
-      } else {
-        if (hit) {
-          if (cacheSecs < 0) " (action cache hit)"  else " (page cache hit)"
-        } else {
-          if (cacheSecs < 0) " (action cache miss)" else " (page cache miss)"
-        }
-      }
-    }
-
     if (e == null) {
-      if (logger.isDebugEnabled) logger.debug(msgWithTime + extraInfo)
+      if (logger.isDebugEnabled) logger.debug(msgWithTime(controller, beginTimestamp) + extraInfo(controller, cacheSecs, hit))
     } else {
-      if (logger.isErrorEnabled) logger.error("Dispatch error " + msgWithTime + extraInfo, e)
+      if (logger.isErrorEnabled) logger.error("Dispatch error " + msgWithTime(controller, beginTimestamp) + extraInfo(controller, cacheSecs, hit), e)
+    }
+  }
+
+  //----------------------------------------------------------------------------
+
+  private def msgWithTime(controller: Controller, beginTimestamp: Long) = {
+    val endTimestamp = System.currentTimeMillis()
+    val dt           = endTimestamp - beginTimestamp
+    val env          = controller.handlerEnv
+
+    controller.remoteIp + " " +
+    controller.request.getMethod + " " +
+    controller.request.getUri + " -> " +
+    ControllerReflection.controllerActionName(controller.handlerEnv.action) +
+    (if (env.uriParams.nonEmpty)        ", uriParams: "        + RequestEnv.inspectParamsWithFilter(env.uriParams       .asInstanceOf[MMap[String, List[Any]]]) else "") +
+    (if (env.bodyParams.nonEmpty)       ", bodyParams: "       + RequestEnv.inspectParamsWithFilter(env.bodyParams      .asInstanceOf[MMap[String, List[Any]]]) else "") +
+    (if (env.pathParams.nonEmpty)       ", pathParams: "       + RequestEnv.inspectParamsWithFilter(env.pathParams      .asInstanceOf[MMap[String, List[Any]]]) else "") +
+    (if (env.fileUploadParams.nonEmpty) ", fileUploadParams: " + RequestEnv.inspectParamsWithFilter(env.fileUploadParams.asInstanceOf[MMap[String, List[Any]]]) else "") +
+    " -> " + controller.response.getStatus.getCode +
+    ", " + dt + " [ms]"
+  }
+
+  private def extraInfo(controller: Controller, cacheSecs: Int, hit: Boolean) = {
+    if (cacheSecs == 0) {
+      if (controller.isResponded) "" else " (async)"
+    } else {
+      if (hit) {
+        if (cacheSecs < 0) " (action cache hit)"  else " (page cache hit)"
+      } else {
+        if (cacheSecs < 0) " (action cache miss)" else " (page cache miss)"
+      }
     }
   }
 }
