b0263491deea98b8d94f0a054a097b75d50570fe
Ngoc Dao <ngocdaothanh@ngoc.local>
2012-10-19 17:28:05 +0900

    Only compress session cookie when it is bigger than 4KB

--- src/main/scala/xitrum/util/SecureBase64.scala
+++ src/main/scala/xitrum/util/SecureBase64.scala
@@ -3,15 +3,36 @@ package xitrum.util
 import xitrum.Config
 
 object SecureBase64 {
-  /** @param forCookie If true, tries to GZIP compress if > 4KB */
+  /** @param forCookie If true, tries to GZIP compress if > 4KB; the result may > 4KB */
   def encrypt(value: Any, forCookie: Boolean = false): String =
     encrypt(value, Config.config.session.secureKey, forCookie)
 
+  /** @param forCookie If true, tries to GZIP compress if > 4KB; the result may > 4KB */
   def encrypt(value: Any, key: String, forCookie: Boolean): String = {
     val bytes           = SeriDeseri.serialize(value)
-    val maybeCompressed = if (bytes.length > 4 * 1024 && forCookie) Gzip.compress(bytes) else bytes
+    val bytesCompressed = (forCookie && bytes.length > 4 * 1024)
+
+    val maybeCompressed = if (bytesCompressed) Gzip.compress(bytes) else bytes
     val encrypted       = Secure.encrypt(maybeCompressed, key)
-    Base64.encode(encrypted)
+    val ret1            = Base64.encode(encrypted)
+
+    if (!forCookie) {
+      // Not cookie, nothing to do
+      ret1
+    } else {
+      // bytes may <= 4KB but the final result may > 4KB!!!
+      if (ret1.length <= 4 * 1024) {
+        ret1
+      } else if (bytesCompressed) {
+        // bytes has been compressed, nothing more can be done to make the final result smaller
+        ret1
+      } else {
+        // bytes has not been compressed, let's try
+        val reallyCompressed = Gzip.compress(bytes)
+        val encrypted        = Secure.encrypt(reallyCompressed, key)
+        Base64.encode(encrypted)
+      }
+    }
   }
 
   /** @param forCookie If true, tries to GZIP uncompress if the input is compressed */
