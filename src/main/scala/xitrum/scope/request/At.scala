09c4bbfd07695a4321971aac9f948574a68f0fb8
Ngoc Dao <ngocdaothanh@kayac.com>
2012-11-23 17:27:04 +0900

    Support Scalate Mustache template

--- src/main/scala/xitrum/scope/request/At.scala
+++ src/main/scala/xitrum/scope/request/At.scala
@@ -1,14 +1,8 @@
 package xitrum.scope.request
 
-import scala.collection.mutable.{Map => MMap}
+import scala.collection.mutable.HashMap
 
 /** Equivalent to @xxx variables of Rails */
-class At {
-  private[this] val map = MMap[String, Any]()
-
-  def apply[T](key: String) = map(key).asInstanceOf[T]
-
-  def update(key: String, value: Any) { map.put(key, value) }
-
-  def isDefinedAt(key: String) = map.isDefinedAt(key)
+class At extends HashMap[String, Any] {
+  def apply[T](key: String): T = super.apply(key).asInstanceOf[T]
 }
--- src/main/scala/xitrum/view/Scalate.scala
+++ src/main/scala/xitrum/view/Scalate.scala
@@ -6,11 +6,17 @@ import org.fusesource.scalate.{Binding, DefaultRenderContext, RenderContext, Tem
 import org.fusesource.scalate.scaml.ScamlOptions
 import org.jboss.netty.handler.codec.serialization.ClassResolvers
 
-import xitrum.{Config, Controller}
+import xitrum.{Config, Controller, Logger}
 
-object Scalate {
-  private[this] val DIR                   = "src/main/view/scalate"
+object Scalate extends Logger {
+  // build.sbt of Xitrum app should have this line:
+  // scalateTemplateDirectory in Compile <<= (baseDirectory) { _ / "src/main/view" }
+  private[this] val TEMPLATE_DIR          = "src/main/view/scalate"
+
+  // build.sbt of Xitrum app should have this line:
+  // scalateBindings += Binding("helper", "xitrum.Controller", true)
   private[this] val CONTROLLER_BINDING_ID = "helper"
+
   private[this] val CONTEXT_BINDING_ID    = "context"
 
   private[this] val classResolver = ClassResolvers.softCachingConcurrentResolver(getClass.getClassLoader)
@@ -20,8 +26,11 @@ object Scalate {
     ret.allowReload   = !Config.isProductionMode
     ScamlOptions.ugly = Config.isProductionMode
     ret.bindings      = List(
-      Binding(CONTEXT_BINDING_ID,    classOf[RenderContext].getName, true),  // import Scalate utilities like "unescape"
-      Binding(CONTROLLER_BINDING_ID, classOf[Controller].getName,    true))  // import current controller
+      // import Scalate utilities like "unescape"
+      Binding(CONTEXT_BINDING_ID,    classOf[RenderContext].getName, true),
+      // import things in the current controller
+      Binding(CONTROLLER_BINDING_ID, classOf[Controller].getName,    true)
+    )
     ret
   }
 
@@ -30,11 +39,25 @@ object Scalate {
    * @param controller will be imported in the template as "helper"
    */
   def renderFile(controller: Controller, relPath: String): String = {
-    val buffer  = new StringWriter
-    val out     = new PrintWriter(buffer)
-    val context = new DefaultRenderContext(relPath, engine, out)
-    context.attributes.update(CONTEXT_BINDING_ID,    context)
-    context.attributes.update(CONTROLLER_BINDING_ID, controller)
+    val buffer     = new StringWriter
+    val out        = new PrintWriter(buffer)
+    val context    = new DefaultRenderContext(relPath, engine, out)
+    val attributes = context.attributes
+
+    // For bindings in engine
+    attributes.update(CONTEXT_BINDING_ID,    context)
+    attributes.update(CONTROLLER_BINDING_ID, controller)
+
+    // Put controller.at to context
+    controller.at.foreach { case (k, v) =>
+      if (k == CONTEXT_BINDING_ID || k == CONTROLLER_BINDING_ID)
+        logger.warn(
+          CONTEXT_BINDING_ID + " and " + CONTROLLER_BINDING_ID +
+          " are reserved key names for controller's \"at\""
+        )
+      else
+        attributes.update(k, v)
+    }
 
     if (Config.isProductionMode) {
       // In production mode, after being precompiled
@@ -50,7 +73,7 @@ object Scalate {
       val template     = klass.asInstanceOf[Class[Template]].newInstance()
       engine.layout(template, context)
     } else {
-      val path = DIR + File.separator + relPath
+      val path = TEMPLATE_DIR + File.separator + relPath
       engine.layout(path, context)
     }
 
