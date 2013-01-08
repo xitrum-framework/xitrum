3e7797c600662263c6ca4fbf0fe445501ab7df44
Ngoc Dao <ngocdaothanh@gmail.com>
2013-01-08 18:33:19 +0900

    Add doc for TemplateEngine

--- src/main/scala/xitrum/view/TemplateEngine.scala
+++ src/main/scala/xitrum/view/TemplateEngine.scala
@@ -3,18 +3,22 @@ package xitrum.view
 import xitrum.Controller
 import xitrum.controller.Action
 
+/** Template engines should extend this trait and implement its methods. */
 trait TemplateEngine {
+  /** Renders the template associated with the action of the controller. */
   def renderTemplate(
     controller: Controller, action: Action,
     controllerName: String, actionName: String,
     options: Map[String, Any]
   ): String
 
+  /** Renders the template associated with the controller. */
   def renderTemplate(
     controller: Controller, controllerClass: Class[_],
     options: Map[String, Any]
   ): String
 
+  /** Renders the template fragment associated with the controller. */
   def renderFragment(
     controller: Controller, controllerClass: Class[_], fragment: String,
     options: Map[String, Any]
