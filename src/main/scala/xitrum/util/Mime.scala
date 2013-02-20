aaa4ef3c477f8f5599b8d66407e119d7dd96a5f8
Ngoc Dao <ngocdaothanh@gmail.com>
2013-02-20 16:48:33 +0900

    https://github.com/klacke/yaws/blob/master/priv/mime.types

--- src/main/scala/xitrum/util/Mime.scala
+++ src/main/scala/xitrum/util/Mime.scala
@@ -4,12 +4,17 @@ import java.io.File
 import javax.activation.MimetypesFileTypeMap
 
 // See
-//   https://github.com/klacke/yaws/blob/master/src/mime.types
+//   https://github.com/klacke/yaws/blob/master/priv/mime.types
 //   http://svn.apache.org/repos/asf/httpd/httpd/trunk/docs/conf/mime.types
 //   http://download.oracle.com/javaee/5/api/javax/activation/MimetypesFileTypeMap.html
 //   src/main/resources/META-INF/mime.types
 object Mime {
   private[this] val map = new MimetypesFileTypeMap
+
+  /** Same as javax.activation.MimetypesFileTypeMap#addMimeTypes */
+  def addMimeTypes(mime_types: String) {
+    map.addMimeTypes(mime_types)
+  }
 
   def get(file: String) = Option(map.getContentType(file))
 
