36699e77488087bdbe0919b358da82bf3f13df18
Ngoc Dao <ngocdaothanh@gmail.com>
2013-04-10 10:48:41 +0900

    Add doc for ActionActor

--- src/main/scala/xitrum/ActionActor.scala
+++ src/main/scala/xitrum/ActionActor.scala
@@ -3,6 +3,11 @@ package xitrum
 import akka.actor.Actor
 import xitrum.handler.HandlerEnv
 
+/**
+ * An actor will be created when there's request. It will be stopped when the
+ * connection is closed or when the response has been sent by respondText,
+ * respondView etc. methods. It is not stopped right away for chunked response.
+ */
 trait ActionActor extends Actor with Action {
   def receive = {
     case env: HandlerEnv =>
