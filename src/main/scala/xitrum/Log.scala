1c45890703fa557cd59e8911a46d0ddd10eb852b
Dao Ngoc <ngoc@Daos-MacBook-Pro.local>
2013-11-27 08:40:48 +0900

    Improve API doc of xitrum.Log

--- src/main/scala/xitrum/Log.scala
+++ src/main/scala/xitrum/Log.scala
@@ -3,16 +3,16 @@ package xitrum
 import org.slf4j.LoggerFactory
 
 /**
- * Although the class name can be determined by sniffing around on the stack:
- * (Thread.currentThread.getStackTrace)(2).getClassName
- *
- * We use a trait for better speed, because getStackTrace is slow.
- *
- * If you don't care about the class name where the log is made, you can call
- * like this directly:
+ * If you don't care about the class name where the log is made, without having
+ * to extend this trait, you can call like this directly:
  * xitrum.Log.debug("msg"), xitrum.Log.info("msg") etc.
  */
 trait Log {
-  /** Log name is inferred from the class name. */
+  // Although the class name can be determined by sniffing around on the stack:
+  // (Thread.currentThread.getStackTrace)(2).getClassName
+  //
+  // We use a trait for better speed, because getStackTrace is slow.
+
+  /** Log name is inferred from name of the class extending this trait. */
   lazy val log = LoggerFactory.getLogger(getClass)
 }
