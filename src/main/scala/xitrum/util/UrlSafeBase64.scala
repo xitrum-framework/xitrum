61bc8ac7b53fb1385a35b6c5ee301b16c6ad2228
Ngoc Dao <ngocdaothanh@gmail.com>
2014-02-28 20:23:03 +0900

    Fix memory leak related to Netty's ByteBuf

--- src/main/scala/xitrum/util/UrlSafeBase64.scala
+++ src/main/scala/xitrum/util/UrlSafeBase64.scala
@@ -34,7 +34,9 @@ object UrlSafeBase64 extends Log {
     try {
       val withPadding = addPadding(base64String)
       val buffer      = B64.decode(Unpooled.copiedBuffer(withPadding, UTF_8), Base64Dialect.URL_SAFE)
-      Some(ByteBufUtil.toBytes(buffer))
+      val bytes       = ByteBufUtil.toBytes(buffer)
+      buffer.release()
+      Some(bytes)
     } catch {
       case NonFatal(e) =>
         log.debug("Could not decode base64 in URL-safe dialect: " + base64String)
