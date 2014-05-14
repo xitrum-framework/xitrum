9086ff66f8c9aec75b2230d164940ed45d811d1d
Ngoc Dao <ngocdaothanh@gmail.com>
2014-05-14 10:41:27 +0900

    BadClientSilencer logs at TRACE level

--- src/main/scala/xitrum/handler/inbound/BadClientSilencer.scala
+++ src/main/scala/xitrum/handler/inbound/BadClientSilencer.scala
@@ -7,13 +7,6 @@ import xitrum.Log
 /**
  * This handler should be put at the last position of the inbound pipeline to
  * catch all exception caused be bad client (closed connection, malformed request etc.).
- *
- * Do nothing if the exception is one of the following:
- * java.io.IOException: Connection reset by peer
- * java.io.IOException: Broken pipe
- * java.nio.channels.ClosedChannelException: null
- * javax.net.ssl.SSLException: not an SSL/TLS record (Use http://... URL to connect to HTTPS server)
- * java.lang.IllegalArgumentException: empty text (Use https://... URL to connect to HTTP server)
  */
 @Sharable
 class BadClientSilencer extends SimpleChannelInboundHandler[Any] with Log {
@@ -22,16 +15,12 @@ class BadClientSilencer extends SimpleChannelInboundHandler[Any] with Log {
   }
 
   override def exceptionCaught(ctx: ChannelHandlerContext, e: Throwable) {
-    if (e.isInstanceOf[java.io.IOException] ||
-        e.isInstanceOf[java.lang.IllegalArgumentException] ||
+    if (e.isInstanceOf[java.io.IOException]                      ||  // Connection reset by peer, Broken pipe
         e.isInstanceOf[java.nio.channels.ClosedChannelException] ||
-        e.isInstanceOf[javax.net.ssl.SSLException] ||
-        e.isInstanceOf[io.netty.handler.codec.DecoderException] ||
+        e.isInstanceOf[io.netty.handler.codec.DecoderException]  ||
+        e.isInstanceOf[java.lang.IllegalArgumentException]       ||  // Use https://... URL to connect to HTTP server
+        e.isInstanceOf[javax.net.ssl.SSLException]               ||  // Use http://... URL to connect to HTTPS server
         e.isInstanceOf[io.netty.handler.ssl.NotSslRecordException])
-    {
-      if (log.isTraceEnabled) log.trace("exceptionCaught", e)
-    } else {
-      log.warn("exceptionCaught", e)
-    }
+      Log.trace("BadClientSilencer caught exception", e)
   }
 }
