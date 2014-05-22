662aee74dc4cbde677fb7c9e661f4a6326668ee0
Ngoc Dao <ngocdaothanh@gmail.com>
2014-05-23 06:26:06 +0900

    Improve Scaladoc for package xitrum

--- src/main/scala/xitrum/package.scala
+++ src/main/scala/xitrum/package.scala
@@ -2,6 +2,21 @@ import org.slf4s.LoggerFactory
 import nl.grons.metrics.scala.InstrumentedBuilder
 import xitrum.metrics.MetricsManager
 
+/**
+ * Things that are usually used by application developers are put in this package
+ * for convenience, because when they want to use XXX, they can simply write:
+ *
+ * {{import xitrum.XXX}}
+ *
+ * To avoid polluting this namespace, things that are utilities should be put in
+ * package xitrum.util, not here.
+ *
+ * Annotations and validators are put to package xitrum.annation and xitrum.validator
+ * because there are many of them. It's better for application developers to write:
+ *
+ * {{import xitrum.annotation._
+ * import xitrum.validator._}}
+ */
 package object xitrum {
   val version = new Version
 
