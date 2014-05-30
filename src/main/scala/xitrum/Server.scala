a6001a08701a683dd311ec385c3893dbc0b1abf2
Ngoc Dao <ngocdaothanh@gmail.com>
2014-05-30 19:52:43 +0900

    Remove outdated comment

--- src/main/scala/xitrum/Server.scala
+++ src/main/scala/xitrum/Server.scala
@@ -31,8 +31,6 @@ object Server extends Log {
    * SSL codec handler will be automatically prepended for HTTPS server.
    */
   def start(httpChannelInitializer: ChannelInitializer[SocketChannel]) {
-    // Don't know why this doesn't work if put above Config.actorSystem
-    //
     // Redirect Akka log to SLF4J
     // http://doc.akka.io/docs/akka/2.3.3/scala/logging.html
     // http://stackoverflow.com/questions/16202501/how-can-i-override-a-typesafe-config-list-value-on-the-command-line
