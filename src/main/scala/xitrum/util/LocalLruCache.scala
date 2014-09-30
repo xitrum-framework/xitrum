543896e797af87878c82d50c2066e00fce075129
Ngoc Dao <ngocdaothanh@gmail.com>
2014-09-30 12:05:44 +0900

    Improve Scaladoc for LocalLruCache

--- src/main/scala/xitrum/util/LocalLruCache.scala
+++ src/main/scala/xitrum/util/LocalLruCache.scala
@@ -9,14 +9,19 @@ import xitrum.Cache
  * Non-threadsafe, non-distributed LRU cache.
  *
  * http://stackoverflow.com/questions/221525/how-would-you-implement-an-lru-cache-in-java-6
- *
- * Xitrum uses this for storing etags for static files. Each web server in a
- * cluster has its own cache of (file path, mtime) -> etag.
  */
 private class NonThreadsafeLocalLruCache[K, V](capacity: Int) extends LinkedHashMap[K, V](capacity + 1, 1.0f, true) {
   protected override def removeEldestEntry(eldest: Entry[K, V]) = size > capacity
 }
 
+/**
+ * Threadsafe, non-distributed LRU cache.
+ *
+ * http://stackoverflow.com/questions/221525/how-would-you-implement-an-lru-cache-in-java-6
+ *
+ * Xitrum uses this for storing etags for static files. Each web server in a
+ * cluster has its own cache of (file path, mtime) -> etag.
+ */
 object LocalLruCache {
   def apply[K, V](capacity: Int) = Collections.synchronizedMap(new NonThreadsafeLocalLruCache[K, V](capacity))
 }
