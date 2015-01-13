081c6127f6156e37415e18234f33b2a4a2e5393b
Ngoc Dao <ngocdaothanh@gmail.com>
2015-01-13 16:51:41 +0900

    #459 Optimize route collecting: Ignore more packages that obviously don't contain routes

--- src/main/scala/xitrum/routing/IgnoredPackages.scala
+++ src/main/scala/xitrum/routing/IgnoredPackages.scala
@@ -19,6 +19,7 @@ private object IgnoredPackages {
     // Others
     relPath.startsWith("akka/") ||
     relPath.startsWith("ch/qos/logback") ||
+    relPath.startsWith("com/beachape/filemanagement") ||
     relPath.startsWith("com/codahale/metrics") ||
     relPath.startsWith("com/esotericsoftware/kryo/") ||
     relPath.startsWith("com/esotericsoftware/reflectasm/") ||
@@ -27,25 +28,31 @@ private object IgnoredPackages {
     relPath.startsWith("com/thoughtworks/paranamer/") ||
     relPath.startsWith("com/twitter/") ||
     relPath.startsWith("com/typesafe/") ||
+    relPath.startsWith("glokka/") ||
     relPath.startsWith("io/netty/") ||
     relPath.startsWith("javassist/") ||
     relPath.startsWith("nl/grons/metrics/") ||
-    relPath.startsWith("org/jboss/") ||
+    relPath.startsWith("org/aopalliance/") ||
     relPath.startsWith("org/apache/") ||
     relPath.startsWith("org/codehaus/commons/") ||
     relPath.startsWith("org/codehaus/janino/") ||
+    relPath.startsWith("org/cyberneko/html/") ||
+    relPath.startsWith("org/fusesource/hawtjni/") ||
     relPath.startsWith("org/fusesource/leveldbjni/") ||
+    relPath.startsWith("org/fusesource/scalamd/") ||
     relPath.startsWith("org/fusesource/scalate/") ||
+    relPath.startsWith("org/jboss/") ||
     relPath.startsWith("org/json4s/") ||
     relPath.startsWith("org/iq80/leveldb") ||
     relPath.startsWith("org/mozilla/") ||
     relPath.startsWith("org/objectweb/asm/") ||
     relPath.startsWith("org/objenesis/") ||
+    relPath.startsWith("org/openid4java/") ||
     relPath.startsWith("org/slf4j/") ||
     relPath.startsWith("org/slf4s/") ||
     relPath.startsWith("org/w3c/") ||
     relPath.startsWith("org/xml/") ||
-    relPath.startsWith("org/uncommons/") ||
+    relPath.startsWith("org/uncommons/maths/") ||
     relPath.startsWith("rx/")
   }
 }
