bb7b25eb27549b4969e9f6874efb030aac2e37a3
Le Chi Linh <linhlc@mobilus.co.jp>
2019-07-11 17:14:49 +0900

    update testcase

--- src/test/scala/xitrum/handler/inbound/ProxyProtocolHandlerTest.scala
+++ src/test/scala/xitrum/handler/inbound/ProxyProtocolHandlerTest.scala
@@ -5,11 +5,22 @@ import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 import java.net.Socket
 
+import xitrum.Action
 import xitrum.Config
 import xitrum.Log
 import xitrum.Server
+import xitrum.SkipCsrfCheck
+import xitrum.annotation.GET
 
 import org.scalatest._
+
+@GET("/test/xff")
+class XFFTestAction extends Action with SkipCsrfCheck {
+  def execute() {
+    respondText("X-Forwarded-For: " + request.headers().get("X-Forwarded-For"))
+  }
+}
+
 class ProxyProtocolHandlerTest extends FlatSpec with Matchers with BeforeAndAfter with Log {
   private val site = "127.0.0.1"
   behavior of "ProxyProtocolV1"
@@ -22,7 +33,7 @@ class ProxyProtocolHandlerTest extends FlatSpec with Matchers with BeforeAndAfte
     Server.stop()
   }
 
-  "Source IP" should "be same with IP in request header" in {
+  "X-Forwarded-For" should "be same with IP in request message" in {
     Config.xitrum.reverseProxy.foreach(r => {
       r.proxyProtocolEnabledOpt.foreach(proxyProtocolEnabled => {
         if (proxyProtocolEnabled) {
@@ -31,22 +42,51 @@ class ProxyProtocolHandlerTest extends FlatSpec with Matchers with BeforeAndAfte
             new OutputStreamWriter(socket.getOutputStream(), "UTF-8"))
           val in = new BufferedReader(
             new InputStreamReader(socket.getInputStream()))
-          out.write("PROXY TCP4 192.168.1.2 192.168.1.3 8080 8000\r\nGET / HTTP/1.1\r\nConnection: close\r\n\r\n")
+          out.write("PROXY TCP4 192.168.1.2 192.168.1.3 8080 8000\r\nGET /my_site/test/xff HTTP/1.1\r\nConnection: close\r\n\r\n")
           out.flush()
           var line:String = null
           val headers = scala.collection.mutable.Map[String, String]()
           while ({line = in.readLine; line != null}) {
             line.split(":").toList match {
               case h:List[String] if h.length > 1 =>
-                headers += (h(0)->h(1))
+                headers += (h(0)->h(1).trim())
               case _=>
+                Log.debug(line)
             }
-            log.debug(line)
           }
-          log.debug(headers.toString())
           out.close()
           in.close()
-          headers.getOrElse("X-Forwarded-Fox", "") should  equal("192.168.1.2")
+          headers.getOrElse("X-Forwarded-For", "") should  equal("192.168.1.2")
+        }
+      })
+
+    })
+    1 should equal(1)
+  }
+  "X-Forwarded-For" should "be same with IP in request header" in {
+    Config.xitrum.reverseProxy.foreach(r => {
+      r.proxyProtocolEnabledOpt.foreach(proxyProtocolEnabled => {
+        if (proxyProtocolEnabled) {
+          val socket = new Socket(site, 8000)
+          val out = new BufferedWriter(
+            new OutputStreamWriter(socket.getOutputStream(), "UTF-8"))
+          val in = new BufferedReader(
+            new InputStreamReader(socket.getInputStream()))
+          out.write("PROXY TCP4 192.168.1.2 192.168.1.3 8080 8000\r\nGET /my_site/test/xff HTTP/1.1\r\nX-Forwarded-For: 192.168.1.4\r\nConnection: close\r\n\r\n")
+          out.flush()
+          var line:String = null
+          val headers = scala.collection.mutable.Map[String, String]()
+          while ({line = in.readLine; line != null}) {
+            line.split(":").toList match {
+              case h:List[String] if h.length > 1 =>
+                headers += (h(0)->h(1).trim())
+              case _=>
+                Log.debug(line)
+            }
+          }
+          out.close()
+          in.close()
+          headers.getOrElse("X-Forwarded-For", "") should  equal("192.168.1.4")
         }
       })
 
