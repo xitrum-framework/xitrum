53a8e83c8d1696cd7cd9d615f137662738db8614
Le Chi Linh <linhlc@mobilus.co.jp>
2019-08-21 20:12:40 +0900

    Refactor

--- src/main/scala/xitrum/handler/inbound/ProxyProtocolHandler.scala
+++ src/main/scala/xitrum/handler/inbound/ProxyProtocolHandler.scala
@@ -1,8 +1,9 @@
 package xitrum.handler.inbound
 
-import xitrum.Config
+import io.netty.channel.Channel
 import io.netty.channel.ChannelHandler.Sharable
-import io.netty.channel.{Channel, ChannelHandlerContext, ChannelInboundHandlerAdapter}
+import io.netty.channel.ChannelHandlerContext
+import io.netty.channel.SimpleChannelInboundHandler
 import io.netty.handler.codec.haproxy.HAProxyMessage
 import io.netty.handler.codec.http.HttpRequest
 import io.netty.util.AttributeKey
@@ -10,13 +11,6 @@ import io.netty.util.AttributeKey
 object ProxyProtocolHandler {
   val HAPROXY_PROTOCOL_MSG: AttributeKey[HAProxyMessage] =
     AttributeKey.valueOf("HAProxyMessage").asInstanceOf[AttributeKey[HAProxyMessage]]
-
-  def setHAProxyMessage(channel: Channel, msg: HAProxyMessage) {
-    channel.attr(HAPROXY_PROTOCOL_MSG).set(msg)
-    // Fix memory leak
-    // https://github.com/netty/netty/pull/9250
-    msg.release()
-  }
 
   def setRemoteIp(channel: Channel, request: HttpRequest) {
     channel.attr(HAPROXY_PROTOCOL_MSG).get() match {
@@ -35,13 +29,9 @@ object ProxyProtocolHandler {
 }
 
 @Sharable
-class ProxyProtocolHandler extends ChannelInboundHandlerAdapter {
-  override def channelRead(ctx: ChannelHandlerContext, msg: Object) {
-    if (Config.xitrum.reverseProxy.get.proxyProtocol && msg.isInstanceOf[HAProxyMessage]) {
-      ProxyProtocolHandler.setHAProxyMessage(ctx.channel, msg.asInstanceOf[HAProxyMessage])
+class ProxyProtocolHandler extends SimpleChannelInboundHandler[HAProxyMessage] {
+  override def channelRead0(ctx: ChannelHandlerContext, msg: HAProxyMessage) {
+      ctx.channel.attr(ProxyProtocolHandler.HAPROXY_PROTOCOL_MSG).set(msg)
       ctx.channel.pipeline.remove(classOf[ProxyProtocolHandler])
-    } else {
-      ctx.fireChannelRead(msg)
-    }
   }
 }
