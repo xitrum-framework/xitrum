6a16fd647c678ad2e3451bd1803760c4abb46831
Konstantin Borisov <smeagol74@gmail.com>
2021-01-19 15:55:00 +0700

    Swagger documentation for BodyParam and Responses with JSON models generated from case classes by reflections (squashed) (#688)
    #688 Swagger documentation for BodyParam and Responses with JSON models generated from case classes by reflections

--- src/main/scala/xitrum/routing/SwaggerActions.scala
+++ src/main/scala/xitrum/routing/SwaggerActions.scala
@@ -6,11 +6,12 @@ import org.json4s.JField
 import org.json4s.JsonAST.{JString, JArray, JValue, JObject}
 import org.json4s.JsonDSL._
 import org.json4s.jackson.JsonMethods
-
 import xitrum.{FutureAction, Config}
 import xitrum.annotation.{First, GET, Swagger, SwaggerTypes}
+import xitrum.annotation.Swagger.JsonField
 
 import scala.collection.immutable.ListMap
+import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 
 @First
@@ -32,6 +33,7 @@ class SwaggerUi extends FutureAction {
 
 /** https://github.com/swagger-api/swagger-spec/blob/master/versions/2.0.md */
 object SwaggerJson {
+
   import SwaggerTypes._
 
   private val SWAGGER_VERSION = "2.0"
@@ -45,6 +47,8 @@ object SwaggerJson {
 
   def json: String = prettyJson
 
+  private lazy val definitions = mutable.Map.empty[String, JValue]
+
   /** Maybe called multiple times in development mode when reloading routes. */
   def reloadFromRoutes(): Unit = {
     prettyJson = JsonMethods.pretty(JsonMethods.render(getSwaggerObject))
@@ -55,10 +59,11 @@ object SwaggerJson {
   private def getSwaggerObject: JValue = {
     val info     = ("title" -> "APIs documented by Swagger") ~ ("version" -> apiVersion)
     val basePath = if (Config.baseUrl.isEmpty) "/" else Config.baseUrl
-    ("swagger"  -> SWAGGER_VERSION) ~
-    ("info"     -> info) ~
-    ("basePath" -> basePath) ~
-    ("paths"    -> getPathsObject)
+    ("swagger"     -> SWAGGER_VERSION) ~
+    ("info"        -> info) ~
+    ("basePath"    -> basePath) ~
+    ("paths"       -> getPathsObject) ~
+    ("definitions" -> getDefinitionsObject)
   }
 
   private def getPathsObject: JValue = {
@@ -72,6 +77,10 @@ object SwaggerJson {
         (path, pathItem)
       }
     JObject(pathToPathItemObjects: _*)
+  }
+
+  private def getDefinitionsObject: JValue = {
+    JObject(definitions.toList: _*)
   }
 
   private def groupSwaggerRoutesByPath: Map[String, Seq[Route]] = {
@@ -203,6 +212,77 @@ object SwaggerJson {
     if (params.isEmpty) None else Some("parameters" -> JArray(params.toList))
   }
 
+  private def _objectField(field: JsonField): (String, JObject) = {
+    field.name -> _autoJsonType(field.tpe, asField = true)
+  }
+
+  private def _addObjectDefinition(tpe: Swagger.JsonType): Unit = {
+    if (tpe.tpe == Swagger.JsonType.tpe.obj) {
+      val key = tpe.name.get
+      if (!definitions.contains(key)) {
+        var definition = ("type" -> "object") ~
+          ("properties" -> JObject(tpe.fields.map(_objectField): _*))
+        val required = tpe.fields.filter(_.isRequired).map(_.name)
+        if (required.nonEmpty)
+          definition = definition ~
+            ("required" -> required)
+        definitions.put(key, definition)
+      }
+    }
+  }
+
+  private def _definitionPath(tpe: Swagger.JsonType): String = s"#/definitions/${tpe.name.get}"
+
+  private def _simpleType(tipe: String, format: String, withoutSchema: Boolean): JObject = {
+    val tpe = ("type" -> tipe) ~
+      ("format" -> format)
+    if (withoutSchema)
+      tpe
+    else "schema" -> tpe
+  }
+
+  private def _simpleArrayType(tipe: String, format: String, withoutSchema: Boolean): JObject = {
+    val tpe = ("type" -> "array") ~
+      ("items" -> _simpleType(tipe, format, withoutSchema = true))
+    if (withoutSchema)
+      tpe
+    else "schema" -> tpe
+  }
+
+  private def _objectArrayType(path: String, withoutSchema: Boolean): JObject = {
+    val tpe = ("type" -> "array") ~
+      ("items" -> ("$ref" -> path))
+    if (withoutSchema)
+      tpe
+    else "schema" -> tpe
+  }
+
+
+  private def _objectType(path: String, withoutSchema: Boolean): JObject = {
+    val tpe = "$ref" -> path
+    if (withoutSchema)
+      tpe
+    else "schema" -> tpe
+  }
+
+  private def _autoJsonType(tpe: Swagger.JsonType, asField: Boolean = false): JObject = {
+    _addObjectDefinition(tpe)
+    tpe.tpe match {
+      case Swagger.JsonType.tpe.obj =>
+        if (tpe.isArray) {
+          _objectArrayType(_definitionPath(tpe), asField)
+        } else {
+          _objectType(_definitionPath(tpe), asField)
+        }
+      case _ =>
+        if (tpe.isArray) {
+          _simpleArrayType(tpe.tpe, tpe.format, asField)
+        } else {
+          _simpleType(tpe.tpe, tpe.format, asField)
+        }
+    }
+  }
+
   private def getParameter(arg: SwaggerParamArg): JValue = {
     val location = arg match {
       case _: SwaggerPathParam   => "path"
@@ -214,35 +294,43 @@ object SwaggerJson {
 
     val required = !arg.isInstanceOf[SwaggerOptParamArg]
 
-    val (tipe, format) = arg match {
-      case _: SwaggerIntParam      => ("integer",  "int32")
-      case _: SwaggerLongParam     => ("integer",  "int64")
-      case _: SwaggerFloatParam    => ("number",  "float")
-      case _: SwaggerDoubleParam   => ("number",  "double")
-      case _: SwaggerStringParam   => ("string",  "")
-      case _: SwaggerByteParam     => ("string",  "byte")
-      case _: SwaggerBinaryParam   => ("string",  "binary")
-      case _: SwaggerBooleanParam  => ("boolean", "")
-      case _: SwaggerDateParam     => ("string",  "date")
-      case _: SwaggerDateTimeParam => ("string",  "date-time")
-      case _: SwaggerPasswordParam => ("string",  "password")
-      case _: SwaggerFileParam     => ("file",    "")
-    }
-
     ("name"        -> arg.name) ~
     ("in"          -> location) ~
     ("description" -> arg.desc) ~
     ("required"    -> required) ~
-    ("type"        -> tipe) ~
-    ("format"      -> format)
+    (arg match {
+      case _: SwaggerIntParam      => _simpleType(Swagger.JsonType.tpe.integer, Swagger.JsonType.fmt.int32,    withoutSchema = true)
+      case _: SwaggerLongParam     => _simpleType(Swagger.JsonType.tpe.integer, Swagger.JsonType.fmt.int64,    withoutSchema = true)
+      case _: SwaggerFloatParam    => _simpleType(Swagger.JsonType.tpe.number,  Swagger.JsonType.fmt.float,    withoutSchema = true)
+      case _: SwaggerDoubleParam   => _simpleType(Swagger.JsonType.tpe.number,  Swagger.JsonType.fmt.double,   withoutSchema = true)
+      case _: SwaggerStringParam   => _simpleType(Swagger.JsonType.tpe.string,  Swagger.JsonType.fmt.none,     withoutSchema = true)
+      case _: SwaggerByteParam     => _simpleType(Swagger.JsonType.tpe.string,  Swagger.JsonType.fmt.byte,     withoutSchema = true)
+      case _: SwaggerBinaryParam   => _simpleType(Swagger.JsonType.tpe.string,  Swagger.JsonType.fmt.binary,   withoutSchema = true)
+      case _: SwaggerBooleanParam  => _simpleType(Swagger.JsonType.tpe.boolean, Swagger.JsonType.fmt.none,     withoutSchema = true)
+      case _: SwaggerDateParam     => _simpleType(Swagger.JsonType.tpe.string,  Swagger.JsonType.fmt.date,     withoutSchema = true)
+      case _: SwaggerDateTimeParam => _simpleType(Swagger.JsonType.tpe.string,  Swagger.JsonType.fmt.dateTime, withoutSchema = true)
+      case _: SwaggerPasswordParam => _simpleType(Swagger.JsonType.tpe.string,  Swagger.JsonType.fmt.password, withoutSchema = true)
+      case _: SwaggerFileParam     => _simpleType(Swagger.JsonType.tpe.file,    Swagger.JsonType.fmt.none,     withoutSchema = true)
+      case _: SwaggerJsonParam     => arg match {
+        case argJson: Swagger.JsonBody => _autoJsonType(argJson.tpe)
+        case _                         => _simpleType(Swagger.JsonType.tpe.string, Swagger.JsonType.fmt.none, withoutSchema = true)
+      }
+    })
   }
+
 
   private def getResponses(args: Seq[SwaggerArg]): Option[JField] = {
     val codeToDescs = args.filter(_.isInstanceOf[Swagger.Response]).map { arg =>
       val response = arg.asInstanceOf[Swagger.Response]
       val code = if (response.code <= 0) "default" else response.code.toString
       val desc = response.desc
-      code -> JObject("description" -> JString(desc))
+      var details = JObject("description" -> JString(desc))
+      response.tpeOpt match {
+        case Some(tpe) =>
+          details = details ~ _autoJsonType(tpe)
+        case None =>
+      }
+      code -> details
     }
     Some("responses" -> JObject(codeToDescs: _*))
   }
